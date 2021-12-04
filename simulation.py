ccdec8634fb2918e6265e75168c343f959814fed
Raihan Rasheed Apurbo <apurbo97@gmail.com>
2021-12-04 06:34:48 +0600

    part b completed

--- simulation.py
+++ simulation.py
@@ -1,9 +1,15 @@
 import logging
-from os import stat
-
-logging.getLogger().setLevel(logging.INFO)
 import random
 import math
+from enum import IntEnum
+import csv
+
+logging.getLogger().setLevel(logging.WARNING)
+
+
+class Server_Status(IntEnum):
+    BUSY = 1
+    IDLE = 0
 
 
 def expexponential_random_variable(mean):
@@ -12,76 +18,7 @@ def expexponential_random_variable(mean):
     return -1 * mean * ln_uniform_random_variable
 
 
-input_file = open("input.txt", "rt")  # r-> read t-> text mode
-output_file = open("output.txt", "wt")  # w-> write t-> text mode
-
-input_line = input_file.readline()
-numbers = input_line.split()
-mean_arrival_time = float(numbers[0])
-mean_service_time = float(numbers[1])
-number_of_total_customers = int(numbers[2])
-logging.info(f"mean arrival time: {mean_arrival_time}")
-logging.info(f"mean service time: {mean_service_time}")
-logging.info(f"number of customer: {number_of_total_customers}")
-
-output_file.write(f"Single-server queueing system\n\n")
-output_file.write(f"mean interarrival time {mean_arrival_time} minutes\n\n")
-output_file.write(f"Mean service time {mean_service_time}\n\n")
-output_file.write(f"Number of customers {number_of_total_customers}\n\n")
-
-from enum import IntEnum
-
-
-class Server_Status(IntEnum):
-    BUSY = 1
-    IDLE = 0
-
-
-# initializing state variables
-
-state = {
-    "sim_time": 0.0,
-    "server_status": Server_Status.IDLE,
-    "number_of_people_in_queue": 0,
-    "last_event_time": 0.0,
-    "next_event_type": 0,
-    "number_of_possible_events": 2,
-}
-
-logging.info(f"initialized state variables {state}")
-
-# initializing counter variables
-
-counter = {
-    "number_of_customer_delayed": 0,
-    "total_of_delays": 0.0,
-    "area_number_in_queue": 0.0,
-    "area_server_status": 0.0,
-}
-
-logging.info(f"initialized counter variables {counter}")
-
-
-# initializing event list
-
-time_next_event = []
-# departure event is in 0th index
-time_next_event.append(None)
-# next interarrival time is in 1th index
-time_next_event.append(
-    state["sim_time"] + expexponential_random_variable(mean_arrival_time)
-)
-# next service time is in 2nd index
-time_next_event.append(float("inf"))
-
-# arrival time of every customer will be stored in this array assigning them with None
-arrival_time_list = [None] * (number_of_total_customers + 1)
-
-
-logging.info(f"initialized event array {time_next_event}")
-
-
-def timing():
+def timing(state, time_next_event, output_file):
     min_time_next_event = float("inf")
     state["next_event_type"] = 0
 
@@ -101,7 +38,7 @@ def timing():
     state["sim_time"] = min_time_next_event
 
 
-def update_counters():
+def update_counters(state, counter):
     time_since_last_event = state["sim_time"] - state["last_event_time"]
     state["last_event_time"] = state["sim_time"]
     counter["area_number_in_queue"] += (
@@ -110,7 +47,16 @@ def update_counters():
     counter["area_server_status"] += int(state["server_status"]) * time_since_last_event
 
 
-def arrive():
+def arrive(
+    state,
+    counter,
+    time_next_event,
+    arrival_time_list,
+    mean_service_time,
+    mean_arrival_time,
+    number_of_total_customers,
+    output_file,
+):
     # some one arriving so we have to schedule new arrival time for next person
     time_next_event[1] = state["sim_time"] + expexponential_random_variable(
         mean_arrival_time
@@ -136,7 +82,7 @@ def arrive():
         )
 
 
-def depart():
+def depart(state, counter, time_next_event, arrival_time_list, mean_service_time):
     # if queue is empty make the status IDLE and set departure to infinity
     if state["number_of_people_in_queue"] == 0:
         state["server_status"] = Server_Status.IDLE
@@ -159,9 +105,10 @@ def depart():
             arrival_time_list[i] = arrival_time_list[i + 1]
 
 
-def report():
-    logging.info(f"state: {state}")
-    logging.info(f"counters: {counter}")
+def report(state, counter, output_file):
+    logging.info(f"state after completion: {state}")
+    logging.info(f"counter values after completion: {counter}")
+
     average_delay = counter["total_of_delays"] / counter["number_of_customer_delayed"]
     output_file.write(f"\n\nAverage delay in queue {average_delay}\n\n")
 
@@ -174,17 +121,148 @@ def report():
     output_file.write(f"Time simulation ended {state['sim_time']}")
 
 
-while counter["number_of_customer_delayed"] < number_of_total_customers:
+def simulate(mean_arrival_time, mean_service_time, number_of_total_customers):
 
-    timing()
+    output_file = open("output.txt", "wt")  # w-> write t-> text mode
 
-    update_counters()
+    logging.info(f"mean arrival time: {mean_arrival_time}")
+    logging.info(f"mean service time: {mean_service_time}")
+    logging.info(f"number of customer: {number_of_total_customers}")
 
-    if state["next_event_type"] == 1:
-        arrive()
-    elif state["next_event_type"] == 2:
-        depart()
+    output_file.write(f"Single-server queueing system\n\n")
+    output_file.write(f"mean interarrival time {mean_arrival_time} minutes\n\n")
+    output_file.write(f"Mean service time {mean_service_time}\n\n")
+    output_file.write(f"Number of customers {number_of_total_customers}\n\n")
 
-report()
-input_file.close()
-output_file.close()
+    # initializing state variables
+
+    state = {
+        "sim_time": 0.0,
+        "server_status": Server_Status.IDLE,
+        "number_of_people_in_queue": 0,
+        "last_event_time": 0.0,
+        "next_event_type": 0,
+        "number_of_possible_events": 2,
+    }
+
+    logging.info(f"initialized state variables {state}")
+
+    # initializing counter variables
+
+    counter = {
+        "number_of_customer_delayed": 0,
+        "total_of_delays": 0.0,
+        "area_number_in_queue": 0.0,
+        "area_server_status": 0.0,
+    }
+
+    logging.info(f"initialized counter variables {counter}")
+
+    # initializing event list
+
+    time_next_event = []
+    # departure event is in 0th index
+    time_next_event.append(None)
+    # next interarrival time is in 1th index
+    time_next_event.append(
+        state["sim_time"] + expexponential_random_variable(mean_arrival_time)
+    )
+    # next service time is in 2nd index
+    time_next_event.append(float("inf"))
+
+    # arrival time of every customer will be stored in this array assigning them with None
+    arrival_time_list = [None] * (number_of_total_customers + 1)
+
+    logging.info(f"initialized event array {time_next_event}")
+
+    while counter["number_of_customer_delayed"] < number_of_total_customers:
+
+        timing(state, time_next_event, output_file)
+
+        update_counters(state, counter)
+
+        if state["next_event_type"] == 1:
+            arrive(
+                state,
+                counter,
+                time_next_event,
+                arrival_time_list,
+                mean_service_time,
+                mean_arrival_time,
+                number_of_total_customers,
+                output_file,
+            )
+        elif state["next_event_type"] == 2:
+            depart(
+                state, counter, time_next_event, arrival_time_list, mean_service_time
+            )
+
+    report(state, counter, output_file)
+    output_file.close()
+
+    return state, counter
+
+
+def part_a():
+    input_file = open("input.txt", "rt")  # r-> read t-> text mode
+
+    input_line = input_file.readline()
+    numbers = input_line.split()
+    mean_arrival_time = float(numbers[0])
+    mean_service_time = float(numbers[1])
+    number_of_total_customers = int(numbers[2])
+    input_file.close()
+
+    simulate(mean_arrival_time, mean_service_time, number_of_total_customers)
+
+
+def part_b():
+    input_file = open("input.txt", "rt")  # r-> read t-> text mode
+
+    input_line = input_file.readline()
+    numbers = input_line.split()
+    mean_arrival_time = float(numbers[0])
+    mean_service_time = float(numbers[1])  # this is not needed for part_b
+    number_of_total_customers = int(numbers[2])
+    input_file.close()
+
+    csv_file = open("report.csv", "wt")
+    csv_writer = csv.writer(csv_file)
+    header = [
+        "k",
+        "average delay in queue",
+        "average number in queue",
+        "server utilization",
+        "time the simulation ended",
+    ]
+    csv_writer.writerow(header)
+
+    k_values = [0.5, 0.6, 0.7, 0.8, 0.9]
+
+    for k in k_values:
+        state, counter = simulate(
+            mean_arrival_time, k * mean_arrival_time, number_of_total_customers
+        )
+        logging.warning(f"iteration : {k}")
+        logging.warning(f"state after completion: {state}")
+        logging.warning(f"counter values after completion: {counter}")
+
+        average_delay = (
+            counter["total_of_delays"] / counter["number_of_customer_delayed"]
+        )
+        average_length = counter["area_number_in_queue"] / state["sim_time"]
+        server_utilization = counter["area_server_status"] / state["sim_time"]
+
+        row = [
+            str(k),
+            str(average_delay),
+            str(average_length),
+            str(server_utilization),
+            state["sim_time"],
+        ]
+        csv_writer.writerow(row)
+
+    csv_file.close()
+
+
+part_b()
